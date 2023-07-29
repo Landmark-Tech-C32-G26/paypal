f71ef4f591dd024dabc4b2b0fe689c415ebe15ad
Zino Uche <emazino@yahoo.com>
2023-07-29 00:57:19 -0300

    testing done for sshkey gen

--- app.java
+++ app.java
@@ -2,3 +2,4 @@ This is application written in java
 Testing git stash
 This app is excellent
 This app is excellent for all users
+This app is excellent for all users, testing ssh key authentication
